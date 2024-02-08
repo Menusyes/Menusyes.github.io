a1cb7e1814eca74a30561845aabc1e880e90ecf6
yangbo <menus_bo@163.com>
2024-02-08 14:19:39 +0800

    调整

--- blog/js/src/crash_cheat.js
+++ blog/js/src/crash_cheat.js
@@ -1,3 +1,3 @@
 // build time:Tue Jan 07 2020 15:15:28 GMT+0800 (GMT+08:00)
-var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","/images/apple-touch-icon-next.png");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
+var OriginTitle=document.title;var titleTime;document.addEventListener("visibilitychange",function(){if(document.hidden){$('[rel="icon"]').attr("href","/img/TEP.ico");document.title="╭(°A°`)╮ 页面崩溃啦 ~";clearTimeout(titleTime)}else{$('[rel="icon"]').attr("href","blog/images/apple-touch-icon-next.png");document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle;titleTime=setTimeout(function(){document.title=OriginTitle},2e3)}});
 //rebuild by neat 
